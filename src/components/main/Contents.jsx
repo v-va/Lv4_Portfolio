5a9b7d4abb9f02ced7701742af95a0f2e542c093
gigupc11 <gigupc11@naver.com>
2023-05-02 18:55:44 +0900

    feat:모달 생성

--- src/components/main/Contents.jsx
+++ src/components/main/Contents.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from '../common/Modal'
+import Button from '../common/Button'
 
 const Contents = () => {
+  const [modal,setModal] = useState(false)
+
+
   return (
     <div>
+    {modal === true? <Modal/>:null}
+
+    {modal && <Modal onClose = {() => {setModal(false)}}/>}
+    
       메인 본문 내용입니다.
+      <Button.ButtonA size='large' onClick = {() => {setModal(true)}}>새 글 작성</Button.ButtonA>
+
     </div>
   )
 }
