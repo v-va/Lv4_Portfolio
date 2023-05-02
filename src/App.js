f53988b349e7b5aae8fb8eeb6c907e3282996e59
v-va <poruch0915@gmail.com>
2023-05-02 15:57:29 +0900

    Design : Globalstyle 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
-import Router from './shared/Router';
+import "./App.css";
+import GlobalStyle from "./components/common/GlobalStyle";
+import Router from "./shared/Router";
 
 function App() {
-  return <Router />;
+    return (
+      <>
+          <GlobalStyle />
+          <Router />
+      </>
+  );
 }
 
 export default App;
